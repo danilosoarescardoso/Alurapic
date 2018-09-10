4803db41ae44d542a0d6ce117a5b1e24d27601a5
Danilo Soares Cardoso <dsc.soares@gmail.com>
2018-09-10 20:46:12 -0300

    Refatorando e utilizando CSS

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <li v-for="foto of fotos" :key="foto">
-      <img :src="foto.url" :alt="foto.titulo">
-    </li>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto">
+
+        
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -17,11 +20,37 @@ export default {
   },
 
   created(){
-    alert('Criei o componente');
+    let promise = this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+
   }
 }
 </script>
 
 <style>
+
+.corpo{
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado{
+  text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+  .imagem-responsiva{
+    width: 100%;
+  }
+
+ 
 
 </style>

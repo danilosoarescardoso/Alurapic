fce1ff8232bff98db05d52016b4bd1b26d567aea
Danilo Soares Cardoso <dsc.soares@gmail.com>
2018-09-14 23:06:00 -0300

    Criando rotas

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="corpo">
+
+    <nav>
+      <ul>
+        <li v-for="route in routes" :key="route.path"> 
+          <router-link :to="route.path ? route.path : '/' ">{{ routes.titulo }}</router-link>
+        </li>
+      </ul>
+    </nav>
+   
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 
+import { routes } from './routes';
 
 export default {  
+
+  data(){
+    return{
+      routes
+    }
+  }
 }
 </script>
 
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,8 @@ import Home from './components/home/Home.vue';
 
 export const routes = [
     
-    {path: '', component: Home}, 
-    {path: '/cadastro', component: Cadastro}
+    {path: '', component: Home, titulo:'Home'}, 
+    {path: '/cadastro', component: Cadastro, titulo: 'Cadastro'}
 
 ];
 
